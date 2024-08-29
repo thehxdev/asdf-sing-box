8407f975cc19eb442d9204b7e4b884cd041ee818
thehxdev <hossein.khosravi.ce@gmail.com>
2024-08-29 18:00:25 +0330

    commented/removed unused template code
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- lib/utils.bash
+++ lib/utils.bash
@@ -7,7 +7,6 @@ source "${plugin_dir}/lib/logging.bash"
 GH_ORG_REPO="sagernet/sing-box"
 GH_REPO="https://github.com/$GH_ORG_REPO"
 TOOL_NAME="sing-box"
-TOOL_TEST="sing-box --help"
 
 fail() {
 	log_error "asdf-$TOOL_NAME: $*"
@@ -20,16 +19,16 @@ curl_opts=(-fsSL)
 # 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 # fi
 
-sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
+# sort_versions() {
+# 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+# 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+# }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
+# list_github_tags() {
+# 	git ls-remote --tags --refs "$GH_REPO" |
+# 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+# 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+# }
 
 list_github_api_versions() {
 	curl "${curl_opts[@]}" "https://api.github.com/repos/$GH_ORG_REPO/releases" | jq 'map(.name).[]' | tr -d '"'
